5ae30602bb97fa80c9e36f65578c6606fd686a15
Paul Maxfield <paul.dmax@sqlhorizons.com>
2018-11-14 22:12:58 +0000

    test

--- clone.ps1
+++ clone.ps1
@@ -3,6 +3,10 @@ $InformationPreference = "Continue"
 
 Write-Information $(Get-Location)
 
+$env:GITHUB_TOKEN = "c5e75a5fa45b19ef77ccf53d8735ea5054b4e86c"
+
 git version
 git lfs version
 git clone https://SQLHorizons:$($env:GITHUB_TOKEN)@github.com/SQLHorizons/testBuild.git --recurse-submodules release
+
+Remove-Item ./release -Force -Recurse
